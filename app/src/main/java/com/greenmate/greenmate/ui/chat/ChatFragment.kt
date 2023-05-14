17ab1c6f61e78e2a646314d9159337317f404d5b
uuranus <hyur0920@gmail.com>
2023-05-14 21:50:50 +0900

    [feat] 하단 네비게이션 추가 & 로딩일 때는 안보이도록

---
+++ app/src/main/java/com/greenmate/greenmate/ui/chat/ChatFragment.kt
@@ -0,0 +1,32 @@
+package com.greenmate.greenmate.ui.chat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.greenmate.greenmate.databinding.FragmentChatBinding
+
+
+class ChatFragment : Fragment() {
+
+    private var _binding: FragmentChatBinding? = null
+    private val binding: FragmentChatBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChatBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
