9465f9af41a084b2bcbc0a14dadf67d06b50b26b
uuranus <hyur0920@gmail.com>
2023-05-22 14:53:51 +0900

    [feat] 로그인 화면들 레이아웃 세팅

---
+++ app/src/main/java/com/greenmate/greenmate/ui/addGreenMate/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.greenmate.greenmate.ui.addGreenMate.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.greenmate.greenmate.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
