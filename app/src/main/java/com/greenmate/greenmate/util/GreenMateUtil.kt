79b81ddc59d0a406fdbd49d7c87e9f183fc6dea7
uuranus <hyur0920@gmail.com>
2023-05-29 18:22:48 +0900

    [feat] 캡쳐를 위해 데이터 일부 수정

--- app/src/main/java/com/greenmate/greenmate/util/GreenMateUtil.kt
+++ app/src/main/java/com/greenmate/greenmate/util/GreenMateUtil.kt
@@ -5,15 +5,15 @@ import com.greenmate.greenmate.R
 
 fun decideLight(light: Int): String {
     return when (light) {
-        in 200..1500 -> "좋음"
-        in 0..200 -> "낮음"
+        in 2..1500 -> "좋음"
+        in 0..1 -> "낮음"
         else -> "높음"
     }
 }
 
 fun decideHumidity(humidity: Int): String {
     return when (humidity) {
-        in 40..70 -> "좋음"
+        in 40..100 -> "좋음"
         in 0..40 -> "낮음"
         else -> "높음"
     }
@@ -29,8 +29,8 @@ fun decideTemperature(temp: Int): String {
 
 fun decideSoilWater(soilWater: Int): String {
     return when (soilWater) {
-        in 400..500 -> "좋음"
-        in 0..400 -> "낮음"
+        in 0..500 -> "좋음"
+        in -100..0 -> "낮음"
         else -> "높음"
     }
 }
